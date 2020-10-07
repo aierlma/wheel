a2bf2af1d145214f9a839663cc0ca28f7e42f128
aierlma <55280375+aierlma@users.noreply.github.com>
2020-10-07 17:32:47 +0800

    change file filter

--- create_and_move.py
+++ create_and_move.py
@@ -2,23 +2,20 @@
 """
 Created on Sun Jun 21 17:33:10 2020
 
-@author: QGX
+@author: Aierlma
 """
 
 import os
 import shutil
 
 a = os.getcwd()
-path = a 
-filelist = os.listdir(path) #该文件夹下所有的文件（包括文件夹）
-for file in filelist:
-    if file == "1.TXT" or file == 'a.bat' or file == 'create_and_move.py':
-        continue
+FORMAT = ['.avi','.mp4',".mkv"] # 要依据的格式
+file_names = [name for name in os.listdir(a) for item in FORMAT if os.path.splitext(name)[1] == item]
+for file in file_names:
     Olddir=os.path.join(path,file)   #原来的文件路径
     if os.path.isdir(Olddir):   #如果是文件夹则跳过
         continue
     filename=os.path.splitext(file)[0]   #文件名
     print (filename)
     os.mkdir(a+'\\'+filename)
-    
     shutil.move(Olddir, a+'\\'+filename)
